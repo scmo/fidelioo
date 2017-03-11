cb54fca2f20a8df362dcafa53853f2e1da4b3d70
joelsonderegger <json@bluewin.ch>
2017-03-11 17:01:27 +0100

    added static folder

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,18 @@
 $(function () {
 	var socket = io();
-	socket.on('person entered', function(user){
+	socket.on('person entered', function(users){
 		var str = JSON.stringify(user);
 	//$('#user').text(str);
 
-		$('.userinstore-wrapper').append('<div class="user-wrapper active"> <div style="height: 100px;"> <div class="user-foto-wrapper"> <img src="./img/user/1.png"> </div> <div class="user-description-wrapper"> Eric Almquist </div> </div> <div class="user-details-wrapper">Last Visit: 14.3.2017 14:30</div> </div>');
+		for ( i = 0; i<users.length; i++) {
+			
+			$('.userinstore-wrapper').append('<div class="user-wrapper"> <div style="height: 100px;"> <div class="user-foto-wrapper"> <img src="./img/user/1.png"> </div> <div class="user-description-wrapper"> Eric Almquist </div> </div> <div class="user-details-wrapper">Last Visit: 14.3.2017 14:30</div> </div>');
+			
+			if (users[0].isNew = true) {
+				$('.user-wrapper').addClass('active');
+			}
+
+		}
 
 	});
 });
