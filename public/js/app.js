d577128aee2ca9e15d4c8ade8792317c5a56859d
joelsonderegger <json@bluewin.ch>
2017-03-11 17:18:29 +0100

    users comes as array

--- public/js/app.js
+++ public/js/app.js
@@ -5,13 +5,21 @@ $(function () {
 	//$('#user').text(str);
 
 		for ( i = 0; i<users.length; i++) {
-			
-			$('.userinstore-wrapper').append('<div class="user-wrapper"> <div style="height: 100px;"> <div class="user-foto-wrapper"> <img src="./img/user/1.png"> </div> <div class="user-description-wrapper"> Eric Almquist </div> </div> <div class="user-details-wrapper">Last Visit: 14.3.2017 14:30</div> </div>');
+
+
+			var isNewCustomer = false;
+			if(users[0].visits.length == 0) {
+				isNewCustomer = true;
+			}
+			$('.userinstore-wrapper').append('<div class="user-wrapper"> <div style="height: 100px;"> <div class="user-foto-wrapper"> <img src="./img/user/1.png"> </div> <div class="user-description-wrapper">'+ users[0].prename +' '+ users[0].lastname +'</div> </div> <div class="user-details-wrapper">Last Visit: 14.3.2017 14:30</div> </div>');
 			
 			if (users[0].isNew = true) {
 				$('.user-wrapper').addClass('active');
 			}
 
+			if (isNewCustomer = true) {
+				$('.badge-newcustomer').show();
+			}
 		}
 
 	});
