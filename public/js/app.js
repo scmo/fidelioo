3dafd3fd5b2aff9171fcfb04a2e8a3392756bc70
joelsonderegger <json@bluewin.ch>
2017-03-11 17:37:51 +0100

    added badge

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,8 @@
+$( document ).ready(function() {
+    // TODO: get users
+});
+
+
 $(function () {
 	var socket = io();
 	socket.on('person entered', function(users){
@@ -12,7 +17,7 @@ $(function () {
 			if(users[0].visits.length == 0) {
 				isNewCustomer = true;
 			}
-			$('.userinstore-wrapper').append('<div class="user-wrapper"> <div style="height: 100px;"> <div class="user-foto-wrapper"> <img src="./img/user/1.png"> </div> <div class="user-description-wrapper">'+ users[0].prename +' '+ users[0].lastname +'</div> </div> <div class="user-details-wrapper">Last Visit: 14.3.2017 14:30</div> </div>');
+			$('.userinstore-wrapper').append('<div class="user-wrapper"> <div style="height: 100px;"> <div class="user-foto-wrapper"> <img src="./img/user/1.png"> </div> <div class="user-description-wrapper">'+ users[0].prename +' '+ users[0].lastname +'</div><div class="badge-newcustomer"><img src="./img/new_badge.png"></div> <br style="clear: left;" /> </div> <div class="user-details-wrapper">Last Visit: 14.3.2017 14:30</div> </div>');
 			
 			if (users[0].isNew = true) {
 				$('.user-wrapper').addClass('active');
