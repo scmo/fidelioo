ec840ae3eb30d80d518b8d1055bd75275fd537e0
joelsonderegger <json@bluewin.ch>
2017-03-12 10:43:14 +0100

    magic

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,7 @@
 function loadCustomersInStore(customers) {
   var str = JSON.stringify(customers);
 		$('.user-wrapper').remove();
+
 		console.log(str);
 	//$('#user').text(str);
 
@@ -15,28 +16,54 @@ function loadCustomersInStore(customers) {
 
 
       var userWrapper = $('<div/>', {class: 'user-wrapper'});
-      userWrapper.append('<div style="position: relative; z-index:1;"> <div class="user-foto-wrapper"> <img src="./img/user/'+ customers[i].uid +'.png"> </div> <div class="user-description-wrapper">'+ customers[i].prename +' '+ customers[i].lastname +'<div class="badge-newcustomer"><span class="badge badge-success firsttimeuserbadge">First Time Visitor</span><span class="last-visit">Last Visit: '+ moment.unix((customers[i].visits[0])).format('MM.DD.YYYY') +'</span></div></div><br style="clear: left;" /> </div> <div class="user-details-wrapper">Arrived: '+ moment.unix((customers[i].arrivedAt)).format('HH:mm') +'<br>Number of past visits: '+ customers[i].visits.length +'<br>Birthday '+ moment.unix((customers[i].birthdate)).format('MM.DD.YYYY') +'<br></div>');
+      userWrapper.append('<div style="position: relative; z-index:1;"> <div class="user-foto-wrapper"> <img src="./img/user/'+ customers[i].uid +'.png"> </div> <div class="user-description-wrapper">'+ customers[i].prename +' '+ customers[i].lastname +'<span class="badge badge-primary brithdaybadge" style="float:right;">Birthday</span><div class="badge-newcustomer"><span class="badge badge-success firsttimeuserbadge">First Time Visitor</span><span class="last-visit">Last Visit: '+ moment.unix((customers[i].visits[0])).format('MM.DD.YYYY') +'</span></div></div><br style="clear: left;" /> </div> <div class="user-details-wrapper">Arrived: '+ moment.unix((customers[i].arrivedAt)).format('HH:mm') +'<br>Number of past visits: '+ customers[i].visits.length +'<br>Birthday '+ moment.unix((customers[i].birthdate)).format('MM.DD.YYYY') +'<br></div>');
 
+
+       $('.userinstore-wrapper').append(userWrapper);
+       
        if (customers[i].justEntered){
          userWrapper.addClass( "just-entered" );
+          userWrapper.find(".user-details-wrapper").show();
+      
       }
-      $('.userinstore-wrapper').append(userWrapper);
+     
+     
       $('div.just-entered').css("display", "none");
       $('div.just-entered').slideDown();
 
 
 
+
+			if (customers[i].isNew == true) {
+				console.log("new");
+				$('.user-wrapper').last().addClass('active');
+			} else {
+				console.log("not new");
+				$('.user-wrapper').last().removeClass('active');
+			}
 			
 			// check if customers birthday
-			if (isNewCustomer == true) {
-				$('.last-visit').last().hide();
-				$('.firsttimeuserbadge').last().show();
+			var birthday = new Date(moment.unix((customers[i].birthdate)).format("MM/DD/YYYY"));
+			var today = new Date();
+			console.log(birthday)
+
+			var hasBirthday = false;
+			birthday.setYear(2017);
+			today.setYear(2017);
+			if(birthday.setHours(0,0,0,0) == today.setHours(0,0,0,0)) {
+				hasBirthday = true;
+			 }
+
+			if (hasBirthday == true) {
+
+				$('.brithdaybadge').last().show();
 			} else {
-				$('.last-visit').last().show();
-				$('.firsttimeuserbadge').last().hide();
+
+				$('.brithdaybadge').last().hide();
 			}
 
 
+			//check if customer new
 			if (isNewCustomer == true) {
 				$('.last-visit').last().hide();
 				$('.firsttimeuserbadge').last().show();
